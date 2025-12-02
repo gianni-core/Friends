f992e61445ce0e449f8104666b110ed03afcda13
𝓖∘𝕧𝕠𝕚𝖉𝖆 <zikmuytb3@gmail.com>
2025-12-02 19:31:14 +0100

    Update data.js

--- data.js
+++ data.js
@@ -18,15 +18,6 @@ const usersData = [
         blocage: 0
     },
     {
-        identifiant: 'BunnyFunnyBestship',
-        password: 'G14nn1LEGOAT',
-        points: 88,
-        birthday: '03-04',
-        notes: '👉 Occupée, Flemme de beaucoup de chose ect...',
-        numberOfCrosses: 0,
-        blocage: 0
-    },
-    {
         identifiant: 'Jo2000',
         password: 'XXX2025/2026',
         points: 87,
@@ -50,42 +41,6 @@ const usersData = [
         points: 168,
         birthday: '07-05',
         notes: 'A besoin de me faire un peu plus confiance et moins au gens que tu connais pas et ne pas croire trop les infulenceurs.',
-        numberOfCrosses: 0,
-        blocage: 0
-    },
-    { 
-        identifiant: 'Papa',
-        password: '!ticket',
-        points: 0, // Inactif - 70
-        birthday: '07-07',
-        notes: 'Passionné d’informatique, de montage et de jeux vidéo, plutôt sympa.',
-        numberOfCrosses: 3,
-        blocage: 1
-    },
-    {
-        identifiant: 'MariDfx',
-        password: '1234',
-        points: 79,
-        birthday: '08-19',
-        notes: '🎪 Stalkeuse, fan de cirque, apparemment BE, Bon courage',
-        numberOfCrosses: 0,
-        blocage: 0
-    },
-    {
-        identifiant: 'Sim',
-        password: 'simsim36785_',
-        points: 87,
-        birthday: '12-24',
-        notes: 'A surement du Potentiel , et très gentil voir trop parfois, réactif',
-        numberOfCrosses: 0,
-        blocage: 0
-    },
-    {
-        identifiant: 'Kiwi143',
-        password: 'BonsoirParis25',
-        points: 70,
-        birthday: '06-27',
-        notes: 'Pour l’instant, tu es très gentil, pas trop trop interactive a besoin que on vienne lui parler pour pouvoir engager une disscusion.',
         numberOfCrosses: 0,
         blocage: 0
     },
